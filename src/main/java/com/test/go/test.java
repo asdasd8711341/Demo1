95c59db97cfb754b1eee9a49cad4be1f197becc8
asdasd8711341 <995736488@qq.com>
2020-10-07 16:22:59 +0800

    第一次提交

---
+++ src/main/java/com/test/go/test.java
@@ -0,0 +1,9 @@
+package com.test.go;
+
+import com.sun.swing.internal.plaf.synth.resources.synth_sv;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
